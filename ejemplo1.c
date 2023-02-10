d1dfab3fb6fbf85a1fb92ab8a9d22226040e4519
Iván Álvarez Fernández <124284729+ivanaf03@users.noreply.github.com>
2023-02-10 20:48:54 +0100

    Add files via upload

---
+++ ejemplo1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <threads.h>
+
+int run(void* args){ //Función a ejecutar
+int* a=args;
+    printf("El hilo está ejecutandose con el argumento %i\n", *a);
+    (*a)++;
+    return (*a);
+}
+
+int main(){
+
+    thrd_t t; //ID del thread
+    int i=1; //Variable del thread
+    int result; //Solución
+
+    thrd_create(&t, run, &i);
+    thrd_join(t, &result);
+
+    printf("El resultado es %i\n", result);
+
+    return 0;
+}
